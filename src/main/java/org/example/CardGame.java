9afef6e2b2f7be526a2b5e69775c63b4f69125ce
Henry Adams <henrybruce.adams@icloud.com>
2023-05-22 16:29:30 +0100

    updated cardgame

--- src/main/java/org/example/CardGame.java
+++ src/main/java/org/example/CardGame.java
@@ -3,18 +3,19 @@ package org.example;
 import java.util.ArrayList;
 
 public class CardGame {
-    public String dealCard() {
-        return cards.get(0).toString();
-    }
-
-
-    public String getDeck() {
-        for (Card card : cards) {
-            System.out.println(card);
+    private static int getSymbolValue(String symbol) {
+        if (symbol.equals("A")) {
+            return 14;
+        } else if (symbol.equals("K")) {
+            return 13;
+        } else if (symbol.equals("Q")) {
+            return 12;
+        } else if (symbol.equals("J")) {
+            return 11;
+        } else {
+            return Integer.parseInt(symbol);
         }
-        return null;
     }
-
 
     public static void main(String[] args) {
         ArrayList<Card> cards = new ArrayList<>();
@@ -28,25 +29,10 @@ public class CardGame {
                 cards.add(new Card(symbol, suit, value));
             }
         }
+
         // print all cards
         for (Card card : cards) {
             System.out.println(card);
         }
-
-        private static int getSymbolValue(String symbols) {
-            if (symbols.equals("A")) {
-                return 14;
-            } else if (symbols.equals("K")) {
-                return 13;
-            } else if (symbols.equals("Q")) {
-                return 12;
-            } else if (symbols.equals("J")) {
-                return 11;
-            } else {
-                return Integer.parseInt(symbols);
-            }
-        }
-
-
     }
 }
