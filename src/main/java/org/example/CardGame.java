1801d48be5416e4a8733e7bbea53ae5a7301b1ad
Henry Adams <henrybruce.adams@icloud.com>
2023-05-23 15:54:58 +0100

    Can now draw the first card

--- src/main/java/org/example/CardGame.java
+++ src/main/java/org/example/CardGame.java
@@ -3,22 +3,23 @@ package org.example;
 import java.util.ArrayList;
 
 public class CardGame {
-
-
     public static void main(String[] args) {
         ArrayList<Card> cards = new ArrayList<>();
 
         String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
         String[] suits = {"\u2665", "\u2663", "\u2666", "\u2660"};
 
-        for(String suit : suits) {
+        for (String suit : suits) {
             for (String symbol : symbols) {
                 int value = getSymbolValue(symbol);
                 cards.add(new Card(symbol, suit, value));
             }
         }
         getDeck(cards);
+        System.out.println("Player is about to draw a card...");
+        dealCard(cards);
     }
+
     private static int getSymbolValue(String symbol) {
         if (symbol.equals("Ace")) {
             return 14;
@@ -33,12 +34,18 @@ public class CardGame {
         }
     }
 
-
-    public static void getDeck(ArrayList<Card> cards) {
+    private static void getDeck(ArrayList<Card> cards) {
         for (Card card : cards) {
             System.out.println(card);
         }
     }
+
+    private static void dealCard(ArrayList<Card> cards) {
+        Card topCard = cards.get(0);
+        System.out.println("They drew a card: " + topCard);
+    }
+
+
 
 
 
