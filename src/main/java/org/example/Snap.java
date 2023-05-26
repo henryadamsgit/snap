14070beddaf2d2c921da0f1a0765d6ba2f6649be
Henry Adams <henrybruce.adams@icloud.com>
2023-05-26 14:28:29 +0100

    Added functionality to play again or quit when game ends.

--- src/main/java/org/example/Snap.java
+++ src/main/java/org/example/Snap.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
-    public class Snap extends CardGame {
-        ArrayList<Card> cards;
-        boolean isPlayer1Turn;
+public class Snap extends CardGame {
+    ArrayList<Card> cards;
+    boolean isPlayer1Turn;
 
-        public Snap(ArrayList<Card> cards) {
-            this.cards = cards;
-            this.isPlayer1Turn = true;
-        }
+    public Snap(ArrayList<Card> cards) {
+        this.cards = cards;
+        this.isPlayer1Turn = true;
+    }
 
-        public void playGame() {
+    public void playGame() {
+        boolean playAgain = true;
+
+        while (playAgain) {
             final boolean[] hasWon = {false};
             Card previousCard = null;
 
@@ -50,9 +53,7 @@ import java.util.TimerTask;
                         hasWon[0] = true;
                         break;
                     } else {
-                        System.out.println("Incorrect input. Player " + (isPlayer1Turn ? "1" : "2") + " LOSES!");
-                        hasWon[0] = true;
-
+                        System.out.println("Incorrect input. Player " + (isPlayer1Turn ? "1" : "2") + " loses the round.");
                     }
                 }
 
@@ -66,5 +67,13 @@ import java.util.TimerTask;
             if (!hasWon[0]) {
                 System.out.println("YOU RAN OUT OF CARDS TO MATCH! GAME OVER");
             }
+
+            System.out.println("Would you like to play again? Enter 'yes' to restart or enter 'quit' to exit.");
+            String restartInput = userInput.nextLine();
+            playAgain = restartInput.isEmpty() || restartInput.equalsIgnoreCase("yes");
+            if (!playAgain) {
+                System.out.println("Thank you for playing SNAP!");
+            }
         }
     }
+}
